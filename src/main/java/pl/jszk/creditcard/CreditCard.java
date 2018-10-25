04c5afb04194392156a6e2b8c4c379d238078110
Albert Stapor <askooler1@gmail.com>
2018-10-25 08:07:58 +0000

    Initial commit

---
+++ src/main/java/pl/jszk/creditcard/CreditCard.java
@@ -0,0 +1,11 @@
+package pl.jszk.creditcard;
+
+class CreditCard {
+    public void assignLimit(double money) {
+        
+    }
+    
+    public double getLimit() {
+        return 2000;
+    }
+}
